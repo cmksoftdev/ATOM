b3da5575a35abbc04b6e8d63967ee96603e3ac33
cmk1988 <cmk88@web.de>
2018-10-25 22:13:59 +0200

    b64 encoded test

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -12,7 +12,11 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            var asm = GetAssemblyFromUrl("http://cmk.bplaced.net/download/TestAssembly.dll");
+            var asm = GetAssemblyFromUrl("http://cmk.bplaced.net/download/Test.b64", x => 
+            {
+                var array = Encoding.ASCII.GetChars(x);
+                return Convert.FromBase64CharArray(array, 0, array.Length);
+            });
             var callableClass = Get<ICallable>("TestAssembly", "Class1", asm, null);
             callableClass.Call();
             Console.ReadLine();
